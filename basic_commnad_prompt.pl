b096343dd2cbb2d04ea88c15781c4cb6448e844c
Mindrican <Mihai.Mindrican@elektrobit.com>
2016-08-08 10:45:36 +0300

    4 commands

--- basic_commnad_prompt.pl
+++ basic_commnad_prompt.pl
@@ -1,44 +1,62 @@
-$base_location='E:\workspace\learning_perl';
+open($f,'>D:/workspace/learning_perl/command_history.txt');
+$base_location='D:\workspace\learning_perl';
 print "Copy Started\n";
-print "$base_location";
-$command=<STDIN>;
 $number_of_commands_accepted=5;
-while($number_of_commands_accepted)
+while($number_of_commands_accepted>0)
 {
-    chomp $command;
-    if($command eq "cd ..")
-    {
-        @path_array=split /\\/,$base_location;
-        pop(@path_array);
-        $temp_location=join("\\",@path_array);
-        $base_location="$temp_location";
-    }
-    elsif($command eq "dir")
-    {
-       opendir $dir,$base_location;
-       my @files=readdir $dir;
-       closedir $dir;
-       $number_of_files=@files;
-       print "Total number of files: $number_of_files \n\n";
-       
-       foreach $file (@files)
-       {
-            print "$file \n";
-       }
-    }
-    # elsif($command eq "cd ")
-    # {
-        # @path_array=split /\\/,$base_location;
-        # push @path_array,$command;
-        # $temp_location=join("\\",@path_array);
-        # $base_location="$temp_location";
-    # }
-    else
+	print "$base_location>";
+	$command=<STDIN>;
+	chomp $command;
+	@words_in_commnad=split ' ',$command;
+	$len=@words_in_commnad;
+	if($len<2)
+	{
+		$command="$command _";
+	}
+	foreach $var (@words_in_commands)
+	{
+		print "$var\n";
+	}
+	if ($words_in_commnad[0] eq 'cd')
+	{
+		if($words_in_commnad[1] eq '..')
+		{
+			@path_array=split /\\/,$base_location;
+			pop(@path_array);
+			$temp_location=join("\\",@path_array);
+			$base_location="$temp_location";	
+		}
+		else
+		{
+			@path_array=split /\\/,$base_location;
+			push @path_array, $words_in_commnad[1];
+			$temp_location=join("\\",@path_array);
+			$base_location="$temp_location";	
+		}
+	}
+	elsif($words_in_commnad[0] eq 'dir')
+	{
+		opendir $dir,$base_location;
+		my @files=readdir $dir;
+		closedir $dir;
+		$number_of_files=@files;
+		print "Total number of files: $number_of_files \n\n";
+
+		foreach $file (@files)
+		{
+			print "$file \n";
+		}
+	}
+	elsif ($words_in_commnad[0] eq 'mkdir')
+	{
+		mkdir("$base_location\\$words_in_commnad[1]");
+	}
+	else
     {
         print "Command not recognize \n";
     }
-    print $base_location;
-    $command=<STDIN>;
+	print $f "$words_in_commnad[0]:$words_in_commnad[1]\n";
     $number_of_commands_accepted--;
 }
+close $f;
 print "Copy ended succesfuly"
